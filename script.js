e4629c13c38ff03ea079a64ad57182d9244a7ba3
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-29 19:45:04 +0330

    indented with spaces

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function computeLoan() {
-	const amount = document.getElementById("amount").value;
-	const interestRate = document.getElementById("interestRate").value;
-	const months = document.getElementById("months").value;
+  const amount = document.getElementById("amount").value;
+  const interestRate = document.getElementById("interestRate").value;
+  const months = document.getElementById("months").value;
 
-	const interest = (amount * (interestRate * 0.01)) / months;
-	const payment = (amount / months + interest).toFixed(2);
-	const output = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	document.getElementById("payment").innerText = `Monthly Payment = $${output}`;
+  const interest = (amount * (interestRate * 0.01)) / months;
+  const payment = (amount / months + interest).toFixed(2);
+  const output = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  document.getElementById("payment").innerText = `Monthly Payment = $${output}`;
 }
